96cc282ce736baaa0e3b020415be95ca3b6008ac
hope4848 <hope4849@naver.com>
2026-06-23 14:35:41 +0900

    feat: create spring boot coin api

--- app/coin-api/src/main/java/com/project/coinapi/controller/CoinController.java
+++ app/coin-api/src/main/java/com/project/coinapi/controller/CoinController.java
@@ -1,4 +1,15 @@
 package com.project.coinapi.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
 public class CoinController {
+
+    @GetMapping("/")
+    public Map<String, String> home() {
+        return Map.of("service", "coin-price-api", "status", "running");
+    }
 }
---
+++ app/coin-api/src/test/java/com/project/coinapi/CoinApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.coinapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoinApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
